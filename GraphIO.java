b81df9a43e2f7009c406185dcd9ac79c6597368b
OwenCorriganUCD <62837888+OwenCorriganUCD@users.noreply.github.com>
2022-03-31 14:26:31 +0100

    Write Text File To Graph (hashmap)

--- GraphIO.java
+++ GraphIO.java
@@ -1,10 +1,10 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class GraphIO {
 
     public void writeToFile() {
-        String path = nameFile();
+        String path = fileName();
         File graph = new File(path);
         BufferedWriter bf = null;
 
@@ -25,7 +25,17 @@ public class GraphIO {
         }
     }
 
-    public String nameFile() {
+    public void writeToHashmap() {
+        // read text file to HashMap
+        Map<String, String> mapFromTxtFile = HashMapFromTextFile();
+
+        // iterate over HashMap entries
+        for (Map.Entry<String, String> entry : mapFromTxtFile.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    public String fileName() {
         Scanner scn = new Scanner(System.in);
         String name = "Graphs/" + scn.nextLine() + ".txt";
         return name;
