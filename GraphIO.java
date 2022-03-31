185adb521f17de6a9d1e47484bd5265ff28cd268
OwenCorriganUCD <62837888+OwenCorriganUCD@users.noreply.github.com>
2022-03-31 15:39:11 +0100

    Check If File Already Exists
    Added code to fileName so that the user doesn't try to use a file name that already exists

--- GraphIO.java
+++ GraphIO.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class GraphIO {
 
     public void writeToFile() {
-        String path = fileName();
+        String path = fileName(true);
         File graph = new File(path);
         BufferedWriter bf = null;
 
@@ -25,19 +25,59 @@ public class GraphIO {
         }
     }
 
-    public void writeToHashmap() {
-        // read text file to HashMap
+    public void writeToHashmapMain() {
         Map<String, String> mapFromTxtFile = HashMapFromTextFile();
 
-        // iterate over HashMap entries
         for (Map.Entry<String, String> entry : mapFromTxtFile.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
-    public String fileName() {
-        Scanner scn = new Scanner(System.in);
-        String name = "Graphs/" + scn.nextLine() + ".txt";
+    public Map<String, String> writeToHashMap() {
+        Map<String, String> map = new HashMap<String, String>();
+        BufferedReader br = null;
+        String path = fileName(false);
+
+        try {
+            File file = new File(path);
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                String user = parts[0].trim();
+                Int numRetweets = parts[1].trim();
+                if (!user.equals("") && !numRetweets.equals(""))
+                    map.put(user, numRetweets);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                }
+                ;
+            }
+        }
+        return map;
+    }
+
+    // boolean passed in so that the code can differentiate between creating a new
+    // file and loading a file
+    public String fileName(boolean tf) {
+        boolean exists = tf;
+        while (exists) {
+            Scanner scn = new Scanner(System.in);
+            String name = "Graphs/" + scn.nextLine() + ".txt";
+            File f = new File(name);
+            if (f.exists()) {
+                exists = true;
+            } else {
+                exists = false;
+            }
+        }
         return name;
     }
 }
