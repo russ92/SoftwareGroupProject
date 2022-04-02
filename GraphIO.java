df80a80c8af542217c3d2d26c147abcfbbe517ce
OwenCorriganUCD <62837888+OwenCorriganUCD@users.noreply.github.com>
2022-04-02 19:26:03 +0100

    GraphIO Overhaul
    Bug fixes
    More user input taken

--- GraphIO.java
+++ GraphIO.java
@@ -3,14 +3,43 @@ import java.util.*;
 
 public class GraphIO {
 
-    public void writeToFile() {
-        String path = fileName(true);
+    public static void main(String[] args) {
+
+        System.out.println("GIT");
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("@Potato", 10);
+        map.put("@Poop", 2);
+        map.put("@Tahoe", 69);
+
+        int loop = 1;
+
+        while (loop == 1) {
+            Scanner scn = new Scanner(System.in);
+            System.out.println("1 to write to text file OR 2 to read from text file: ");
+            if (scn.nextLine().equals("1")) {
+                writeToFile(map);
+            }
+            else if (scn.nextLine().equals("2")) {
+                writeToHashMapMain();
+                loop = 0;
+            }
+            else {
+                System.out.println("PLEASE SELECT 1 OR 2");
+            }
+        }
+    }
+
+
+    public static void writeToFile(HashMap<String, Integer> map) {
+        String path = fileName(1);
         File graph = new File(path);
         BufferedWriter bf = null;
 
         try {
-            bf = new BufferWriter(new FileWriter(graph));
-            for (Map.Entry<String, Int> entry : map.entrySet()) {
+            System.out.println("try");
+            bf = new BufferedWriter(new FileWriter(graph));
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
                 bf.write(entry.getKey() + ":" + entry.getValue());
                 bf.newLine();
             }
@@ -25,18 +54,18 @@ public class GraphIO {
         }
     }
 
-    public void writeToHashmapMain() {
-        Map<String, String> mapFromTxtFile = HashMapFromTextFile();
+    public static void writeToHashMapMain() {
+        Map<String, Integer> mapFromTxtFile = writeToHashMap();
 
-        for (Map.Entry<String, String> entry : mapFromTxtFile.entrySet()) {
+        for (Map.Entry<String, Integer> entry : mapFromTxtFile.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
-    public Map<String, String> writeToHashMap() {
-        Map<String, String> map = new HashMap<String, String>();
+    public static Map<String, Integer> writeToHashMap() {
+        Map<String, Integer> map = new HashMap<String, Integer>();
         BufferedReader br = null;
-        String path = fileName(false);
+        String path = fileName(2);
 
         try {
             File file = new File(path);
@@ -46,9 +75,12 @@ public class GraphIO {
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(":");
                 String user = parts[0].trim();
-                Int numRetweets = parts[1].trim();
-                if (!user.equals("") && !numRetweets.equals(""))
+                String numRetweetsStr = parts[1].trim();
+                int numRetweets = Integer.parseInt(numRetweetsStr);
+
+                if (!user.equals("")) {
                     map.put(user, numRetweets);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,18 +96,45 @@ public class GraphIO {
         return map;
     }
 
-    // boolean passed in so that the code can differentiate between creating a new
+
+    // int passed in so that the code can differentiate between creating a new
     // file and loading a file
-    public String fileName(boolean tf) {
-        boolean exists = tf;
-        while (exists) {
-            Scanner scn = new Scanner(System.in);
-            String name = "Graphs/" + scn.nextLine() + ".txt";
-            File f = new File(name);
-            if (f.exists()) {
-                exists = true;
-            } else {
-                exists = false;
+    public static String fileName(int rW) {
+        int readOrWrite = rW;
+        String name = "C:/Users/owenc/OneDrive/UCD Year 3 Sem 2/SWE Project/Git/SoftwareGroupProject/Graphs/"; //CHANGE THIS PATH AS NEEDED. MUST BE EXACT.
+
+        if (readOrWrite == 1) {
+            boolean exists = true;
+            while (exists) {
+                System.out.println("write file");
+                Scanner scn = new Scanner(System.in);
+                System.out.println("ENTER FILE NAME:");
+                name = name + scn.nextLine() + ".txt";
+                File f = new File(name);
+                if (f.exists()) {
+                    exists = true;
+                    System.out.println("FILE ALREADY EXISTS");
+                } 
+                else {
+                    exists = false;
+                }
+            }
+        }
+        else {
+            boolean exists = false;
+            while (!exists) {
+                System.out.println("read file");
+                Scanner scn = new Scanner(System.in);
+                System.out.println("ENTER FILE NAME:");
+                name = name + scn.nextLine() + ".txt";
+                System.out.println(name);
+                File f = new File(name);
+                if (f.exists()) {
+                    exists = true;
+                } else {
+                    exists = false;
+                    System.out.println("FILE DOES NOT EXIST");
+                }
             }
         }
         return name;
