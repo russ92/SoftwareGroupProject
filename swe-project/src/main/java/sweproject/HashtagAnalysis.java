a55a51ed7aa152060f6f70715394152f41f361a3
OwenCorriganUCD <62837888+OwenCorriganUCD@users.noreply.github.com>
2022-04-24 20:48:04 +0100

    Accepting Or Rejecting
    Added method which determines if a sting is accepting or rejecting based on buzz words associated with each stance.
    Only a few buzz words are currently checked.

--- swe-project/src/main/java/sweproject/HashtagAnalysis.java
+++ swe-project/src/main/java/sweproject/HashtagAnalysis.java
@@ -1,21 +1,53 @@
-package sweproject.graph.sprint6;
 
 public class HashtagAnalysis {
 
-    public static String splitCamelCaseHashtag(String hashtag){
+    public static String splitCamelCaseHashtag(String hashtag) {
         StringBuilder hashtagSplit = new StringBuilder();
-        for(int i = 0; i < hashtag.length(); i++) {
+        for (int i = 0; i < hashtag.length(); i++) {
             Character ch = hashtag.charAt(i);
-            if(Character.isUpperCase(ch))
+            if (Character.isUpperCase(ch))
                 hashtagSplit.append(" ").append(Character.toLowerCase(ch));
-            else if(ch != '#')
+            else if (ch != '#')
                 hashtagSplit.append(ch);
         }
         return hashtagSplit.substring(1);
     }
 
-    public static void main(String [] args){
+    public static int acceptingOrRejecting(String hashtag) {
+
+        String[] accepting = { "Praise", "Love", "Hero" };
+        String[] rejecting = { "Fuck", "Hate", "Fire", "Fake" };
+
+        int len = 0;
+        int type = 2;
+        while (len != accepting.length) {
+            if (hashtag.contains(rejecting[len])) {
+                type = 0;
+            }
+            len++;
+        }
+
+        if (type != 2) {
+            while (len != accepting.length) {
+                if (hashtag.contains(accepting[len])) {
+                    type = 1;
+                }
+                len++;
+            }
+        }
+        return type;
+    }
+
+    public static void main(String[] args) {
         String s = "#FauciIsAHero";
-        System.out.println(HashtagAnalysis.splitCamelCaseHashtag(s));
+        String tag = HashtagAnalysis.splitCamelCaseHashtag(s);
+        System.out.println(tag);
+
+        if (acceptingOrRejecting(tag) == 0) {
+            System.out.println("ACCEPTING");
+        } else {
+            System.out.println("REJECTING");
+        }
+
     }
 }
